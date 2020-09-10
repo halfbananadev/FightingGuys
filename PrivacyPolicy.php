47a0d348257dbb78ea797a7f328f9ecadeb2bee5
halfbananadev <71050919+halfbananadev@users.noreply.github.com>
2020-09-10 09:36:10 +0700

    Create PrivacyPolicy.php

---
+++ PrivacyPolicy.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors',1);
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>HalfBanana Dev - Privacy Policy</title>
+</head>
+<body>
+  include 'README.md';
+</body>
+</html>
